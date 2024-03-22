b5d123f761769a99b2db3cc0620a31ef6fa1c8d3
Fendi Mohamed <m.fendi@esi-sba.dz>
2024-03-22 05:31:19 +0100

    add student profile pages

--- app/app/u/s/r/page.tsx
+++ app/app/u/s/r/page.tsx
@@ -1,7 +1,8 @@
 import Profile from "@/components/studentProfile";
+import LeaderBoard from "@/components/studentProfile/leaderBoard";
 
 export default function ProfilePage() {
   return (
-   <Profile/>
+   <LeaderBoard/>
   );
 }
