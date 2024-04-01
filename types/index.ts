1698c66ba3a2a83361a05ae929e228c1e547ac5e
Fendi Mohamed <m.fendi@esi-sba.dz>
2024-04-01 10:51:16 +0200

    add form types

--- types/index.ts
+++ types/index.ts
@@ -14,3 +14,35 @@ export interface SideBarItem {
 }
 
 // Sidebar types <--
+export enum EQuestionType {
+  QCM = "QCM",
+  SORT = "SORT",
+  DRAG_AND_DROP = "DRAG_AND_DROP",
+  QROC = "QROC",
+}
+
+export enum EQuizType {
+  DEVOIR = "DEVOIR",
+  TEST = "TEST",
+}
+
+export enum ECourseType {
+  OOP = "OOP",
+  ANALYSE = "Analyse",
+}
+
+export type Exam = {
+  createdAt: string;
+  id: string;
+  quiz_title: string;
+  quiz_instructions: string;
+  quiz_type: EQuizType;
+  course: ECourseType;
+  quiz_duration: number;
+  question_time_limit: number;
+  questions_count: number;
+  start_date: Date; 
+  end_date: Date; 
+  start_time: string;
+  end_time: string;
+};
