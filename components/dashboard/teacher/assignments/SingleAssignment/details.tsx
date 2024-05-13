a76525ebf6bb15499e2da276b7f6991f2321691d
Fendi Mohamed <m.fendi@esi-sba.dz>
2024-05-13 23:30:24 +0100

    add results assignments table

---
+++ components/dashboard/teacher/assignments/SingleAssignment/details.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+
+interface File {
+  url : string;
+  name : string;
+}
+
+interface QuizDetailsProps {
+  title: string;
+  date: string;
+  subject: string;
+  description : string;
+  files ?: File[];
+}
+
+const Details: React.FC<QuizDetailsProps> = ({
+  title,
+  date,
+  subject,
+  description,
+  files,
+}) => {
+  return (
+    <>
+      <div className="flex flex-col gap-4 justify-start text-lg font-medium" >
+      <div className="flex items-start gap-4">
+       <p className="font-semibold">{title}</p>
+      </div>
+      <div className="flex items-center gap-4">
+      <p className="font-semibold">Assignment Schedule</p>
+        <div className=" bg-[#ededf5] px-2 py-1 rounded-[10px] flex justify-between items-center gap-2 ">
+          <Image
+            src="/dashboard/quiz/calendar.svg"
+            alt="calendar icon"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-4 h-4"
+          />
+          <p className="text-sm font-light">{date} </p>{" "}
+        </div>
+      </div>
+      <div className="flex items-start gap-4">
+        <p className="font-semibold">Subject : </p>
+        <p>{subject} </p>
+      </div>
+      <div className="flex items-start  gap-4">
+          <p className="font-semibold">Description: </p>
+        <p className="max-w-[70ch]" >{description} </p>
+      </div>
+  <table className="w-full border-collapse border border-gray-300 max-w-96 ">
+    <thead>
+      <tr>
+        <th className="py-2 px-4 bg-gray-100 border border-gray-300">File Name</th>
+        <th className="py-2 px-4 bg-gray-100 border border-gray-300">File URL</th>
+      </tr>
+    </thead>
+    <tbody>
+      {files.map((file, index) => (
+        <tr key={index}>
+          <td className="py-2 px-4 border border-gray-300">{file.name}</td>
+          <td className="py-2 px-4 border border-gray-300">
+            <a href={file.url} className="text-blue-500">{file.url}</a>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div> 
+  
+  
+    </>
+  );
+};
+
+export default Details;
---
+++ static/dummy-data/assignment/results.ts
@@ -0,0 +1,66 @@
+export const results = [
+  {
+    id: "1",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+  },
+  {
+    id: "2",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+  },
+  {
+    id: "3",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+    marks: 10,
+  },
+  {
+    id: "1",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+  },
+  {
+    id: "2",
+      name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+  },
+  {
+    id: "3",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+  },
+  {
+    id: "1",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+    marks: 19,
+  },
+  {
+    id: "2",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+  },
+  {
+    id: "3",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+    marks: 15,
+  },
+  {
+    id: "1",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+  },
+  {
+    id: "2",
+      name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+  },
+  {
+    id: "3",
+    name: "Hakim Maroc",
+    file : {url : 'https://miro.com' , name : 'home work' },
+    marks: 12,
+  },
+];
