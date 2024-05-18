944b68c0692f10439bf1c2acbfa8e042bea6e90d
FendiMohamed <m.fendi@esi-sba.dz>
2024-05-18 17:20:17 +0100

    update quiz pass component

--- static/content/Quiz.ts
+++ static/content/Quiz.ts
@@ -1,27 +1,58 @@
 import { Question } from "@/types/detailQuiz";
-export const questions: Question[] = [
+export const questions: any = [
   {
-    id: 1,
-    questionText: "Which of the following is not a networking protocol?",
-    answers: [
-      { id: 1, text: "ICP" },
-      { id: 2, text: "DCP" },
-      { id: 3, text: "CCP" },
-      { id: 4, text: "BCP" },
+    id: '5055',
+    score : 10,
+    body: "Which of the following is not a networking protocol?",
+    options: [
+      { id: '18', option: "ICP" },
+      { id: '29', option: "DCP" },
+      { id: '3489', option: "CCP" },
+      { id: '456', option: "BCP" },
     ],
-    correctAnswerId: 4,
-    selectedAnswerId: 1,
   },
   {
-    id: 2,
-    questionText: "Which of the following is not a networking protocol?",
-    answers: [
-      { id: 1, text: "ICP" },
-      { id: 2, text: "DCP" },
-      { id: 3, text: "CCP" },
-      { id: 4, text: "BCP" },
+    id: '5052',
+    score : 20,
+    body: "Which of the following is not a networking protocol?",
+    options: [
+      { id: '11', option: "ICP" },
+      { id: '2234', option: "DCP" },
+      { id: '35', option: "CCP" },
+      { id: '48', option: "BCP" },
     ],
-    correctAnswerId: 3,
-    selectedAnswerId: 3,
+  },
+  {
+    id: '6666',
+    score : 30,
+    body: "Which of the following is not a networking protocol?",
+    options: [
+      { id: '19', option: "ICP" },
+      { id: '28', option: "DCP" },
+      { id: '37', option: "CCP" },
+      { id: '46', option: "BCP" },
+    ],
+  },
+  {
+    id: '8000',
+    score : 25,
+    body: "Which of the following is not a networking protocol?",
+    options: [
+      { id: '152', option: "ICP" },
+      { id: '22', option: "DCP" },
+      { id: '33', option: "CCP" },
+      { id: '44', option: "BCP" },
+    ],
+  },
+  {
+    id: '9524',
+    score : 15,
+    body: "Which of the following is not a networking protocol?",
+    options: [
+      { id: '188', option: "ICP" },
+      { id: '298', option: "DCP" },
+      { id: '333', option: "CCP" },
+      { id: '444', option: "BCP" },
+    ],
   },
 ];
