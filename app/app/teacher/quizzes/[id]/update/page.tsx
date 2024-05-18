ff73a1c2c5a4c7a0a35137275409a4fb11b3100b
FendiMohamed <m.fendi@esi-sba.dz>
2024-05-18 05:49:37 +0100

    add quiz update page

---
+++ app/app/teacher/quizzes/[id]/update/page.tsx
@@ -0,0 +1,27 @@
+import PreQuiz from "@/components/quiz/PassQuiz";
+import { cookies } from "next/headers";
+import { quiz } from "@/static/dummy-data/quiz";
+import Quiz from "@/components/dashboard/Quiz";
+
+async function getQuiz(id:string) {
+//   const res = await fetch( `quiz/${id}`,{
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${cookies().get("token")}`,
+//    }
+//  })
+ 
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+ 
+  // return res.json()
+  return quiz
+}
+
+export default async function UpdateQuiz({ params }: { params: { id: string } }) {
+  const data = await getQuiz(params?.id)
+
+  return <Quiz quiz={data} />;
+}
