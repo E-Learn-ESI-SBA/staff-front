acae6fd7e2fb8e903bb799c5c89167194dc96b02
RY's <medayb45esi@gmail.com>
2024-05-19 02:17:00 +0100

    add videoForm schema

--- components/forms/video.form.tsx
+++ components/forms/video.form.tsx
@@ -1,8 +1,13 @@
+import {TVideoSchema} from "@/types/chapter/zod";
 
-
-type Props   = {
-    defaultValues:
+type Props = {
+    defaultValues:TVideoSchema
+    sectionId: string;
 }
-export default function VidroForm ( ) {
+export default function VideoForm ({defaultValues,sectionId}:Props ) {
+    return (
+        <form>
 
+        </form>
+    )
 }
--- types/chapter/zod.ts
+++ types/chapter/zod.ts
@@ -1,23 +1,4 @@
 import { z } from "zod";
-
-export const ModuleOverviewSchema = z.object({
-  name: z.string(),
-  id: z.string().optional(),
-  description: z.string().min(120, "description is required"),
-  module_id: z.string(),
-  chapter_number: z.number().optional(),
-  createdAt: z.date().optional().default(new Date()),
-  updatedAt: z.date().optional().default(new Date()),
-  plan: z
-    .array(
-      z.object({
-        value: z.string(),
-      }),
-    )
-    .min(1, "Chapter must have at least one plan"),
-});
-export type TModuleOverviewSchema = z.infer<typeof ModuleOverviewSchema>;
-
 export const SectionFormSchema = z.object({
   name: z.string(),
   id: z.string().optional(),
@@ -57,5 +38,6 @@ export const VideoSchema = z.object({
   name: z.string(),
   id: z.string().optional(),
   url: z.string().url("url is required"),
-  chapter_id: z.string(),
 });
+
+export type TVideoSchema = z.infer<typeof VideoSchema>;
