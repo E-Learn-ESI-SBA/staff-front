57fa1cbf0d6c03d96d6f024c489b3946c2f9d9bb
FendiMohamed <m.fendi@esi-sba.dz>
2024-06-05 22:06:48 +0100

    add create submission

--- app/app/teacher/quizzes/[id]/page.tsx
+++ app/app/teacher/quizzes/[id]/page.tsx
@@ -3,20 +3,21 @@ import { quiz } from "@/static/dummy-data/quiz";
 import Quiz from "@/components/dashboard/Quiz";
 
 async function getQuiz(id: string) {
-  //   const res = await fetch( `quiz/${id}`,{
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Authorization": `Bearer ${cookies().get("token")}`,
-  //    }
-  //  })
+    const res = await fetch( `https://c27c-105-235-137-89.ngrok-free.app/assignments`,{
+      method: "GET",
+      cache : 'no-store',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE4NTUxMzk3LCJpYXQiOjE3MTU5NTkzOTcsImp0aSI6IjYxN2EwNDU3MzNiNDQxNDlhNjY5Y2ZmMjkzOGQ3ZWFlIiwiaWQiOiIyMjNlYmU5Yi1jMWMyLTQ5M2EtYTdiYS02OThhOTM1NjdkYmUiLCJhdmF0YXIiOiJkZWZhdWx0IiwidXNlcm5hbWUiOiJhZG1pbiIsImVtYWlsIjoiYWRtaW5AaG9zdC5jb20iLCJyb2xlIjoiYWRtaW4iLCJncm91cCI6Ik5vbmUiLCJ5ZWFyIjoiTm9uZSJ9.2UFOb8hOBkfnGpWHgkQdJcnbK6YwqbEtn9aIFA-FNBc`,
+     }
+   })
 
-  //   if (!res.ok) {
-  //     throw new Error('Failed to fetch data')
-  //   }
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
 
-  // return res.json()
-  return quiz;
+  return res.json()
+  // return quiz;
 }
 
 export default async function SingleQuiz({
@@ -25,6 +26,6 @@ export default async function SingleQuiz({
   params: { id: string };
 }) {
   const data = await getQuiz(params?.id);
-
-  return <Quiz quiz={data} />;
+  console.log('dd',data)
+  return <Quiz quiz={quiz} />;
 }
--- components/dashboard/Assignment/create/second-step/index.tsx
+++ components/dashboard/Assignment/create/second-step/index.tsx
@@ -79,7 +79,7 @@ const Submission = () => {
     console.log("Submitted form",formData);
   
     try {
-      const response = await fetch("https://89f8-105-235-137-89.ngrok-free.app/assignments", {
+      const response = await fetch("https://c27c-105-235-137-89.ngrok-free.app/assignments", {
         method: "POST",
         headers: {
           Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE4NTUxMzk3LCJpYXQiOjE3MTU5NTkzOTcsImp0aSI6IjYxN2EwNDU3MzNiNDQxNDlhNjY5Y2ZmMjkzOGQ3ZWFlIiwiaWQiOiIyMjNlYmU5Yi1jMWMyLTQ5M2EtYTdiYS02OThhOTM1NjdkYmUiLCJhdmF0YXIiOiJkZWZhdWx0IiwidXNlcm5hbWUiOiJhZG1pbiIsImVtYWlsIjoiYWRtaW5AaG9zdC5jb20iLCJyb2xlIjoiYWRtaW4iLCJncm91cCI6Ik5vbmUiLCJ5ZWFyIjoiTm9uZSJ9.2UFOb8hOBkfnGpWHgkQdJcnbK6YwqbEtn9aIFA-FNBc`
