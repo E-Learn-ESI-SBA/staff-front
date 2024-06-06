a398d271f77e8288095adb598aad4bc0ae5c8c57
poysa <hanaiayoucef@gmail.com>
2024-06-06 19:04:48 +0100

    some improvs in /app/student

--- components/dashboard/student/home/card.tsx
+++ components/dashboard/student/home/card.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Activity, Clock9, Cone } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
@@ -10,9 +11,10 @@ type Props = {
 export default function Card({ title, value, icon }: Props) {
   return (
     <div className="flex justify-start bg-white rounded-3xl text-text w-[450px] p-4 items-center">
-      <Button className="rounded-full bg-secondary-background flex items-center justify-center w-16 h-16 mr-4">
-        <Image src={icon} alt="icon" width={25} height={25} />
+      <Button className="rounded-full bg-secondary-background flex items-center justify-center w-16 h-16 mr-4 text-[#0066FF]">
+        <Icon icon={icon} />
       </Button>
+
       <div className="flex flex-col">
         <p>{title}</p>
         <p className="text-secondary font-semibold text-2xl">{value}</p>
@@ -20,3 +22,20 @@ export default function Card({ title, value, icon }: Props) {
     </div>
   );
 }
+
+
+interface IconProps {
+  icon: any;
+  [key: string]: any;
+}
+
+const icons: Record<string, React.ComponentType<any>> = {
+  activity: Activity,
+  time: Clock9,
+}
+
+
+const Icon: React.FC<IconProps> = ({ icon, ...props }) => {
+  const IconComponent = icons[icon] || Cone;
+  return <IconComponent {...props} />;
+};
--- components/dashboard/student/home/trialClass.tsx
+++ components/dashboard/student/home/trialClass.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
 
 export default function TrialClass() {
@@ -11,12 +12,14 @@ export default function TrialClass() {
         <p className="text-text py-8">
           Time to become advance then <br></br> others with this course{" "}
         </p>
-        <div>
-          <Button className="rounded-full px-8 py-6">Join Trial Class</Button>
-          <Button className="bg-transparent text-text">Skipp</Button>
+        <div className="flex justify-between items-center">
+          <Button className="rounded-full px-8 py-6 hover:bg-white border hover:text-secondary hover:border hover:border-secondary">Join Trial Class</Button>
+          <Button className="bg-transparent text-text hover:bg-white border hover:text-secondary hover:border hover:border-cards-main border-transparent">Skipp</Button>
         </div>
       </div>
-      <div className="bg-secondary-background w-96 rounded-2xl"></div>
+      <div className="bg-secondary-background w-96 rounded-2xl">
+        <Image src="/assets/course.jpeg" width={500} height={500} alt="course" />
+      </div>
     </div>
   );
 }
